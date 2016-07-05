d02f64ab53e35eee3d1ff395c304ad43baa4fe68
Kohei Tabata <nerd0geek1@gmail.com>
2016-07-05 16:30:39 +0900

    apply headerView/headerViewHeight/footerView/footerViewHeight on TableViewDelegate

--- TableViewManager/classes/implementation/TableViewDelegate.swift
+++ TableViewManager/classes/implementation/TableViewDelegate.swift
@@ -31,4 +31,53 @@ public class TableViewDelegate: NSObject, TableViewDelegateType {
 
         return 44
     }
+
+    public func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return sectionData(for: section)?.headerData?.headerHeight ?? 0
+    }
+
+    public func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let sectionData = self.sectionData(for: section) else {
+            return nil
+        }
+
+        let sectionHeaderView = sectionData.headerView
+        if let sectionHeaderView = sectionHeaderView as? SectionHeaderDataAcceptableType, sectionHeaderData = sectionData.headerData {
+            sectionHeaderView.update(sectionHeaderData)
+        }
+
+        return sectionHeaderView
+    }
+
+    public func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return sectionData(for: section)?.footerData?.footerHeight ?? 0
+    }
+
+    public func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        guard let sectionData = self.sectionData(for: section) else {
+            return nil
+        }
+
+        let sectionFooterView = sectionData.footerView
+
+        if let sectionFooterView = sectionFooterView as? SectionFooterDataAcceptableType, sectionFooterData = sectionData.footerData {
+            sectionFooterView.update(sectionFooterData)
+        }
+
+        return sectionFooterView
+    }
+
+    //MARK: - private
+
+    private func sectionData(for section: Int) -> SectionData? {
+        guard let dataSource = dataSource else {
+            return nil
+        }
+
+        if section > dataSource.sectionDataList.count - 1 {
+            return nil
+        }
+
+        return dataSource.sectionDataList[section]
+    }
 }
